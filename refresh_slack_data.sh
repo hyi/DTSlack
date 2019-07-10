21907247b4c392220ade9902ed20d829c6099d37
hyi <hongyi@renci.org>
2019-07-10 09:31:47 -0400

    update refresh data shell script

--- refresh_slack_data.sh
+++ refresh_slack_data.sh
@@ -3,15 +3,15 @@
 # refresh_slack_data: pull data from slack API nightly and time stamp data for later message evolution analysis and visualization
 # Author: Hong Yi
 
-OUTPUT_DATA_PATH=/projects/xdci/slack_data
-BACKUP_PATH=/projects/xdci/slack_data/backup
-WEB_SLACK_PATH=/projects/xdci/www/translator/teamscience/slackviz
+OUTPUT_DATA_PATH=/projects/helx/slack_data
+BACKUP_PATH=/projects/helx/slack_data/backup
+WEB_SLACK_PATH=/projects/helx/www/translator/teamscience/slackviz
 
 echo "*** backing up current working data for visualization ***"
 cp ${WEB_SLACK_PATH}/*.json ${BACKUP_PATH}
 
 echo "*** run python script to pull data using slack API ***"
-SLACK_BOT_TOKEN=$2 python $1/slack_app.py $1 ${OUTPUT_DATA_PATH} inputData.json wordCloud.json rawMessage.txt
+SLACK_BOT_TOKEN=$2 python3.6 $1/slack_app.py $1 ${OUTPUT_DATA_PATH} inputData.json wordCloud.json rawMessage.txt
 echo "*** slack data pull is complete ***"
 
 echo "*** compare the new data with the old data to see whether time stamped data need to be created. Only time stamp data when it is different from the last pull"
